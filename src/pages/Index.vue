5865a2d14e8f9d1851d80631f214dbbd41b95975
maryamdanieza <saripdanieza@gmail.com>
2021-06-01 14:06:39 +0800

    create landing page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,47 +1,27 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>
+    <q-card-section class="q-gutter-lg">
+      <h3 class="q-no-margin q-padding-horizontal">Meranaw Learning App</h3>
+      <q-card class="my-card text-white bg-blue-grey text-white q-pa-md">
+        <q-card-section>
+          <div class="text-h4">Learn</div>
+          <div class="text-subtitle2">Maganad Tano!</div>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card text-white bg-teal-5 text-white q-pa-md">
+        <q-card-section>
+          <div class="text-h4">Game</div>
+          <div class="text-subtitle2">Gitagita Tano!</div>
+        </q-card-section>
+      </q-card>
+    </q-card-section>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ClassComponent.vue';
 import { Vue, Component } from 'vue-property-decorator';
 
-@Component({
-  components: { ExampleComponent }
-})
-export default class PageIndex extends Vue {
-  todos: Todo[] = [
-    {
-      id: 1,
-      content: 'ct1'
-    },
-    {
-      id: 2,
-      content: 'ct2'
-    },
-    {
-      id: 3,
-      content: 'ct3'
-    },
-    {
-      id: 4,
-      content: 'ct4'
-    },
-    {
-      id: 5,
-      content: 'ct5'
-    }
-  ];
-  meta: Meta = {
-    totalCount: 1200
-  };
-};
+@Component({})
+export default class PageIndex extends Vue {}
 </script>
