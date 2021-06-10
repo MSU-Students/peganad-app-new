b6e73c148318bb6d36cfc9b7ced68e321f89c244
maryamdanieza <saripdanieza@gmail.com>
2021-06-10 13:37:11 +0800

    create learn page content

---
+++ src/pages/LearnPageContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-md">
+    <LearnCard />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import LearnCard from 'components/LearnCard.vue';
+
+@Component({
+  components: {
+    LearnCard
+  }
+})
+export default class LearnPageContent extends Vue {}
+</script>
+
+<style></style>
