d4590f1a61d445a4eb3393a053e98ef390175e82
maryamdanieza <saripdanieza@gmail.com>
2021-06-24 16:44:22 +0800

    new location of learn pages

---
+++ src/pages/learn/Learn.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-page class="bg-pink-2 q-pa-md">
+    <div class="sub-header-container q-mt-am">
+      <div class="flex flex-center text-h2" style="height: 250px">
+        <span class="text-h3 text-black">Ready To Learn?</span>
+      </div>
+    </div>
+    <div class="row q-col-gutter-md">
+      <div class="col-6">
+        <q-card class="card bg-deep-purple-5" @click="() => $router.replace('/learn/animals')">
+          <img class="card-img" src="~assets/design/animals.png" />
+          <q-card-section>
+            <div class="text-h6 text-center text-white">Animals</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6">
+        <q-card class="card bg-green-5" @click="() => $router.replace('/learn/colors')">
+          <img class="card-img" src="~assets/design/colors.png" />
+          <q-card-section>
+            <div class="text-h6 text-center text-white">Colors</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6">
+        <q-card class="card bg-blue-5" @click="() => $router.replace('/learn/numbers')">
+          <img class="card-img" src="~assets/design/numbers.png" />
+          <q-card-section>
+            <div class="text-h6 text-center text-white">Numbers</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6">
+        <q-card class="card bg-orange-5" @click="() => $router.replace('/learn/words')">
+          <img  class="card-img" src="~assets/design/words.png" />
+          <q-card-section>
+            <div class="text-h6 text-center text-white">Words</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.sub-header-container {
+  background: url("../../assets/design/study1.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 250px;
+}
+
+.card-img {
+  padding: 10px;
+}
+</style>
